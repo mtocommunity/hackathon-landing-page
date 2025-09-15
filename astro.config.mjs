ea6b570296be521e66fe4bf5a54516c7176f92ca
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2025-09-15 08:35:54 -0500

    staff added, tw fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: { enabled: true },
   }),
   output: "server",
+  vite: {
+    //@ts-expect-error
+    plugins: [tailwindcss()] 
+  },
 });
