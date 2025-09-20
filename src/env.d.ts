3888ecc5d2688a2193d7b115f7043b9f0c0d858a
Luis Bazan <me@luisb.space>
2025-09-20 09:54:34 -0500

    Implement AWS SES to send email confirmation

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,6 +6,9 @@ declare namespace App {
       TURSO_DATABASE_URL: string;
       TURSO_AUTH_TOKEN: string;
       CLODUFLARE_TURNSTILE_SECRET: string;
+      AWS_REGION: string;
+      AWS_ACCESS_KEY_ID: string;
+      AWS_SECRET_ACCESS_KEY: string;
     };
   }
 }
